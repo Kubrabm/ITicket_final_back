d9a45c7a766a3c6868a12ff25dfca9bb3fc952a3
Kubrabm <121182695+Kubrabm@users.noreply.github.com>
2023-09-15 00:47:17 +0400

    init

---
+++ ITicket.DAL/Migrations/20230914203737_initnew.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITicket.DAL.Migrations
+{
+    public partial class initnew : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
